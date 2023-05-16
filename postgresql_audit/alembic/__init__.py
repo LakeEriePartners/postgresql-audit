d6d323c5494ddb1453098ba5a038918d9155d89b
bstreeter <bstreeter@tpastream.com>
2023-05-16 15:21:07 +0000

    quickfix

--- postgresql_audit/alembic/__init__.py
+++ postgresql_audit/alembic/__init__.py
@@ -159,7 +159,7 @@ def add_column_rewrite(context, revision, op):
 
 @writer.rewrites(ops.DropColumnOp)
 def drop_column_rewrite(context, revision, op):
-    column = op._orig_column
+    column = op._reverse.column
     table_info = column.table.info or {}
     if "versioned" in table_info and column.name not in table_info["versioned"].get(
         "exclude", []
